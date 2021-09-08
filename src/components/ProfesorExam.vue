13d605191cc6a5982cb61c24b72ea2c8561cdb8f
marcos <marcos.correa@docato.com.br>
2021-09-08 16:31:29 -0300

    feat: cadastro de provas

--- src/components/ProfesorExam.vue
+++ src/components/ProfesorExam.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="profesor-exam">
-    <!-- First -->
-    <div v-if="stage === 'first'" class="profesor-exam-body">
+    <!-- Home -->
+    <div v-if="stage === 'home'" class="profesor-exam-body">
       <h2 class="title">Provas cadastradas</h2>
 
       <div class="c-white">
@@ -44,177 +44,165 @@
       </div>
     </div>
 
-    <!-- Second -->
-    <div v-if="stage === 'second'" class="profesor-exam-body">
+    <!-- First -->
+    <div v-if="stage === 'first'" class="profesor-exam-body">
       <h2 class="title">Cadastro de prova</h2>
 
       <div class="title">Inicie o cadastro informando os dados abaixo</div>
 
-      <div class="card">
+      <div class="card mt-4">
         <div class="card-body">
-
-          <div class="form-group">  
-            <label for="name">Nome da prova</label>
-            <input id="name" type="text" class="form-control">
-          </div>
-          <div class="form-group">  
-            <label for="subject">Disciplina</label>
-            <input id="subject" type="text" class="form-control">
-
-            <label for="subject">Quantidade de questões</label>
-            <select class="form-select" aria-label="Default select example">
-              <option selected>Open this select menu</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
-          </div>
-       
-       
-
-
-
-
-
-       <v-card
-    :loading="loading"
-    class="mx-auto my-12"
-    max-width="374"
-  >
-    <template slot="progress">
-      <v-progress-linear
-        color="deep-purple"
-        height="10"
-        indeterminate
-      ></v-progress-linear>
-    </template>
-
-    <v-img
-      height="250"
-      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-    ></v-img>
-
-    <v-card-title>Cafe Badilico</v-card-title>
-
-    <v-card-text>
-      <v-row
-        align="center"
-        class="mx-0"
-      >
-        <v-rating
-          :value="4.5"
-          color="amber"
-          dense
-          half-increments
-          readonly
-          size="14"
-        ></v-rating>
-
-        <div class="grey--text ms-4">
-          4.5 (413)
-        </div>
-      </v-row>
-
-      <div class="my-4 text-subtitle-1">
-        $ • Italian, Cafe
-      </div>
-
-      <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
-    </v-card-text>
-
-    <v-divider class="mx-4"></v-divider>
-
-    <v-card-title>Tonight's availability</v-card-title>
-
-    <v-card-text>
-      <v-chip-group
-        v-model="selection"
-        active-class="deep-purple accent-4 white--text"
-        column
-      >
-        <v-chip>5:30PM</v-chip>
-
-        <v-chip>7:30PM</v-chip>
-
-        <v-chip>8:00PM</v-chip>
-
-        <v-chip>9:00PM</v-chip>
-      </v-chip-group>
-    </v-card-text>
-
-    <v-card-actions>
-      <v-btn
-        color="deep-purple lighten-2"
-        text
-        @click="reserve"
-      >
-        Reserve
-      </v-btn>
-    </v-card-actions>
-  </v-card>
-        </div>
-      </div>
-      
-
-
-
-       <!-- <div class="card">
-            <div class="card-body">
-              <h5 class="card-title">{{ prova.title }}</h5>
-              <p class="card-text">{{ prova.description }}</p>
-              <button class="btn btn-primary btn-profesor exam-button">
-                VISUALIZAR PROVA
-              </button>
+          <div class="row">
+            <!-- Nome -->
+            <div class="col-sm-4">
+              <div class="form-group">
+                <label for="name">Nome da prova / Disciplina</label>
+                <input
+                  v-model="examName"
+                  id="name"
+                  type="text"
+                  class="form-control"
+                  placeholder="Ex: Prova de Algorítmos"
+                  required
+                />
+              </div>
             </div>
-          </div> -->
 
-
-
-
-
-      <div class="btn-group-vertical" role="group">
-        <!-- v-for="(alternative, index) of alternatives" -->
-        <div
-          v-for="(alternative, index) of alternatives"
-          :key="index"
-          class="mt-2"
-        >
-          <div class="d-flex">
-            <div>
-              <input
-                type="radio"
-                class="btn-check"
-                name="btnradio"
-                :id="index"
-                autocomplete="off"
-              />
-              <label
-                class="btn btn-outline-success icon-check-square"
-                :for="index"
-              >
-                <!-- aqui -->
-                <b-icon icon="check"></b-icon>
-              </label>
+            <!-- Valor -->
+            <div class="col-sm-4">
+              <div class="form-group">
+                <label for="examvalue">Valor total da prova</label>
+                <input
+                  v-model="examValue"
+                  id="examvalue"
+                  type="number"
+                  class="form-control"
+                  placeholder="Ex: 10"
+                />
+              </div>
             </div>
-            <input type="text" class="form-control" />
+
+            <div class="col-sm-4">
+              <div class="d-flex flex-column">
+                <span class="ms-auto">Valor por questão</span>
+                <h4 class="ms-auto">
+                  {{ (examValue / totalQuestions).toFixed(2) }}
+                </h4>
+              </div>
+            </div>
+
+            <!-- Número de Questões -->
+            <div class="col-sm-12">
+              <div class="form-group">
+                <label for="subject">Quantidade de questões</label>
+
+                <input
+                  v-model="totalQuestions"
+                  min="1"
+                  class="w-100"
+                  max="10"
+                  step="1"
+                  type="range"
+                  value="0"
+                  ref="quest"
+                  @change="setTotalQuestions($event.target.value)"
+                />
+                <div
+                  class="d-flex justify-content-between"
+                  style="padding: 0 0 0 0px"
+                >
+                  <div v-for="(item, index) of quantityArray" :key="index">
+                    <div style="width: 16px; text-align: center">
+                      {{ index + 1 }}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- Descrição	 -->
+            <div class="col-sm-12">
+              <div class="form-group">
+                <label for="description">Descrição da prova</label>
+                <textarea
+                  v-model="examDescription"
+                  name="description"
+                  id="description"
+                  cols="30"
+                  class="form-control"
+                  rows="10"
+                ></textarea>
+              </div>
+            </div>
           </div>
         </div>
       </div>
 
-      <button class="btn btn-">voltar</button>
+      <div class="d-flex justify-content-between mt-4 mb-5">
+        <div>
+          <button class="btn btn-primary btn-profesor btn-large">VOLTAR</button>
+        </div>
+        <div v-if="errors.length > 0">
+          <span v-for="(error, index) of errors" :key="index">
+            <div class="text-danger">{{ error }}</div>
+          </span>
+        </div>
+        <div>
+          <button
+            @click="continueButton('second')"
+            class="btn btn-primary btn-profesor btn-large"
+          >
+            CONTINUAR
+          </button>
+        </div>
+      </div>
     </div>
-  </div>
 
-  <!--  -->
+    <!-- Second -->
+    <div v-if="stage === 'second'" class="profesor-exam-body">
+      <h2 class="title">Questões</h2>
+
+      <div v-if="totalQuestions > 1" class="title">
+        Preencha as {{ totalQuestions }} questões da prova
+      </div>
+      <div v-if="totalQuestions == 1" class="title">
+        Preencha a questão única da prova
+      </div>
+
+      <div class="card mt-4">
+        <div class="card-body">
+          <div v-for="(question, index) of this.questions" :key="index">
+            <QuestionForm
+              :ref="'qform'+index"
+              :indexQuestion="index"
+              v-on:newquestion="valoresFilho(index, $event)"
+            ></QuestionForm>
+          </div>
+        </div>
+      </div>
+
+      <div class="d-flex justify-content-between my-4">
+        <div>
+          <div v-if="!validForm" class="text-danger">Ainda existem campos obrigatórios que não foram preenchidos</div>
+        </div>
+        <button class="btn btn-primary btn-profesor btn-large" @click="criarProva()">CRIAR PROVA</button>
+      </div>
+
+    </div>
+
+    <div class="profesor-exam-body" style="position: relative"></div>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from "vue-property-decorator";
+import { Component, Vue } from "vue-property-decorator";
 import ExamsService from "@/services/ExamsService";
 import { BIcon, BIconArrowUp, BIconArrowDown } from "bootstrap-vue";
+import QuestionForm from "./QuestionForm.vue";
 
 @Component({
   components: {
+    QuestionForm,
     BIcon,
     BIconArrowUp,
     BIconArrowDown,
@@ -222,36 +210,142 @@ import { BIcon, BIconArrowUp, BIconArrowDown } from "bootstrap-vue";
 })
 export default class ProfesorExam extends Vue {
   provas = [];
-  stage = "second";
-  alternatives = [{}, {}, {}, {}, {}];
+  alternatives = [{ description: "", correct: false, marked: false }];
+  errors = [""];
 
-  loading = false
-  selection = 1
+  stage = "home";
+  // First
+  examName = "";
+  examDescription = "";
+  totalQuestions = 1;
+  questions = [{ enunciate: "", alternatives: this.alternatives }];
+  examValue = 10;
 
-  
-  // stage = "first";
-  async created() {
+
+  // Second
+  quantityArray = [{}];
+  newExamObject = {};
+  validForm = true;
+
+  choosedQuestion = 0;
+  criarProva(){
+    if(this.vaLidarForm()){
+      this.montarProva()
+      console.log(this.newExamObject)
+    }
+  }
+  vaLidarForm() {
+    let valid = true
+    for(let i = 0; i< this.questions.length;i++){
+      let element = eval(`this.$refs.qform${i}`)
+      element[0].validarQuest()
+      if(!element[0].isValidQuestion){
+        valid = false
+      }
+    }
+    this.validForm = valid
+    return valid
+  }
+  setTotalQuestions(event: any) {
+    let array = [];
+    for (let i = 0; i < event; i++) {
+      array.push({ enunciate: "", alternatives: this.alternatives });
+    }
+    this.questions = array;
+    console.log('TOTAL QUESTÕES:',this.questions.length);
+  }
+  async created(): Promise<any> {
+    // tirar
+    // this.montarQuestoes();
+
+    for (let i = this.quantityArray.length; i < 10; i++) {
+      this.quantityArray.push({});
+    }
+    for (let i = this.alternatives.length; i < 5; i++) {
+      this.alternatives.push({
+        description: "",
+        correct: false,
+        marked: false,
+      });
+    }
     this.provas = await ExamsService.getAllExams();
   }
-  newExam() {
+  valoresFilho(index: number, event: string) {
+    let question = JSON.parse(event);
+    this.questions[index] = question;
+    console.table(question);
+  }
+  newExam(): void {
+    this.stage = "first";
+  }
+
+
+  resetFirst(): void {
+    this.examName = "";
+    this.examDescription = "";
+    this.totalQuestions = 1;
+    this.examValue = 10;
+  }
+
+  firstContinue(): void {
     this.stage = "second";
   }
-  firstContinue() {
-    this.stage = "third";
-  }
 
-  reserve () {
-    this.loading = true
-
-    setTimeout(() => (this.loading = false), 2000)
+  continueButton(nextstage: string): void {
+    if (nextstage == "second") {
+      if (this.validProvaFirstStage()) {
+        this.stage = nextstage;
+        this.montarProva();
+      }
+    }
+    if (nextstage == "third") {
+     this.criarProva()
+    }
   }
-  
-  
+  validProvaFirstStage(): boolean {
+    this.errors = [];
+    if (
+      this.examName &&
+      this.examDescription &&
+      this.examValue &&
+      this.totalQuestions
+    ) {
+      return true;
+    }
+    if (!this.examName) {
+      this.errors.push("A prova precisa ter um nome");
+    }
+    if (!this.examDescription) {
+      this.errors.push("A prova precisa ter uma descrição");
+    }
+    if (!this.examValue) {
+      this.errors.push("A prova precisa ter um valor total");
+    }
+
+    return false;
+  }
+  // montarQuestoes() {
+  //   for (let i = 0; i < this.totalQuestions; i++) {
+  //     this.questions.push({ enunciate: "", alternatives: this.alternatives });
+  //   }
+  // }
+
+  montarProva(): void {
+    this.newExamObject = {
+      name: this.examName,
+      description: this.examDescription,
+      value: this.examValue,
+      totalQuestions: this.totalQuestions,
+      valuePerQuestion: (this.examValue / this.totalQuestions).toFixed(2),
+      questions:this.questions
+    };
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import "@/assets/scss/_mixins.scss";
 // .user-area {
 //   height: 100vh;
 //   &-cont {
@@ -273,6 +367,9 @@ export default class ProfesorExam extends Vue {
   font-size: 14px;
   height: 32px;
   line-height: 14px;
+  &.btn-large {
+    @include largebutton();
+  }
 }
 .btn-profesor:hover {
   background-color: #04ac18;
---
+++ src/components/QuestionForm.vue
@@ -0,0 +1,219 @@
+<template>
+  <div class="question-form">
+    <div class="form-group my-4">
+      <h4>Questão {{ indexQuestion + 1 }}</h4>
+      <label for="enunciate">Enunciado da questão</label>
+      <div class="text-danger" v-if="errors['enunciate']">
+        É necessário preencher o enunciado
+      </div>
+
+      <textarea
+        name="enunciate"
+        id="index"
+        cols="30"
+        rows="5"
+        class="form-control"
+        @change="setEnunciate($event.target.value)"
+      ></textarea>
+      <!-- alternativas -->
+      <div class="btn-group-vertical" role="group">
+        <div class="form-group mt-3">
+          <label>Preencha as alternativas e selecione qual é a correta</label>
+          <div class="text-danger" v-if="errors['alternatives']">
+            É necessário preencher todas as alternativas
+          </div>
+          <div class="text-danger" v-if="errors['correct']">
+            É necessário marcar uma alternativa correta
+          </div>
+          <div
+            v-for="(alternative, indexAlternative) of alternatives"
+            :key="indexAlternative"
+            class="mt-2"
+          >
+            <div class="d-flex">
+              <div>
+                <input
+                  name="enunciado"
+                  type="radio"
+                  class="btn-check rounded-0"
+                  :id="indexQuestion + 'en' + indexAlternative"
+                  autocomplete="off"
+                  @change="onChange($event, indexAlternative)"
+                />
+                <label
+                  class="btn btn-outline-success icon-check-square rounded-0"
+                  :for="indexQuestion + 'en' + indexAlternative"
+                >
+                  <b-icon icon="check"></b-icon>
+                </label>
+              </div>
+              <input
+                @change="setDescription($event.target.value, indexAlternative)"
+                type="text"
+                class="form-control form-alternative-inputtext rounded-0"
+                v-bind:class="{ 'btn-success': isChecked(indexAlternative) }"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+
+
+<script lang="ts">
+import { Component, Emit, Model, Prop, Vue } from "vue-property-decorator";
+import { BIcon, BIconArrowUp, BIconArrowDown } from "bootstrap-vue";
+
+@Component({
+  components: {
+    BIcon,
+    BIconArrowUp,
+    BIconArrowDown,
+  },
+})
+export default class QuestionForm extends Vue {
+  @Prop({ default: 0 }) private indexQuestion!: number;
+  @Emit("newquestion") public newquestion() {
+    let question = JSON.stringify(this.question);
+    return question;
+  }
+  alternatives = [{ description: "", correct: false, marked: false }];
+  question = { enunciate: "", alternatives: this.alternatives };
+  errors = { correct: false, alternatives: false, enunciate: false };
+  isValidQuestion = false
+
+  async created(): Promise<any> {
+    for (let i = this.alternatives.length; i < 5; i++) {
+      this.alternatives.push({
+        description: "",
+        correct: false,
+        marked: false,
+      });
+    }
+  }
+  public validarQuest(){
+    if(this.validQuestion()){
+      this.emitirQuestao()
+    }
+  }
+  emitirQuestao() {
+    this.validQuestion();
+    if(this.isValidQuestion){
+      this.newquestion();
+    }else{
+      return
+    }
+  }
+  setEnunciate(value: string): void {
+    this.question.enunciate = value;
+    this.validQuestion();
+  }
+
+  onChange(event: any, indexAlternatives: number): void {
+    this.alternatives.forEach((alt) => {
+      alt.correct = false;
+    });
+    this.alternatives[indexAlternatives].correct = event.target.checked;
+    this.validQuestion();
+  }
+  isChecked(index: number): boolean {
+    if (this.alternatives[index].correct) {
+      return true;
+    }
+
+    return false;
+  }
+  setDescription(event: any, index: number): void {
+    this.alternatives[index].description = event;
+    this.validQuestion();
+  }
+
+
+
+  resetErrors() {
+    this.errors["correct"] = false;
+    this.errors["alternatives"] = false;
+    this.errors["enunciate"] = false;
+  }
+  validEnunciate() {
+    if (!this.question.enunciate) {
+      this.errors["enunciate"] = true;
+      return false;
+    }
+    return true;
+  }
+  validAlternatives() {
+    this.alternatives.forEach((alt) => {
+      if (!alt.description) {
+        this.errors["alternatives"] = true;
+        return false
+      }
+    });
+    return true
+  }
+  validCorrect() {
+    let missedCorrect = true;
+    this.alternatives.forEach((alt) => {
+      if (alt.correct) {
+        missedCorrect = false;
+      }
+    });
+    if (missedCorrect) {
+      this.errors["correct"] = true;
+      return false
+    }
+    return true
+  }
+  validQuestion() {
+    this.resetErrors();
+    if (!this.validEnunciate() || !this.validCorrect() || !this.validAlternatives() ) {
+      return false;
+    }
+    this.isValidQuestion = true
+    return true
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "@/assets/scss/_mixins.scss";
+
+.btn-student,
+.btn-profesor {
+  font-size: 14px;
+  height: 32px;
+  line-height: 14px;
+  &.btn-large {
+    @include largebutton();
+  }
+}
+.btn-profesor:hover {
+  background-color: #04ac18;
+}
+.profesor-exam-body {
+  position: absolute;
+  top: 12vh;
+  padding: 0 15vw;
+  width: 100vw;
+  .title {
+    color: white;
+  }
+}
+.exam-button {
+  display: flex;
+  font-size: 14px;
+  width: 200px;
+  height: 32px;
+  justify-content: center;
+  align-items: center;
+  margin-left: auto;
+}
+.history-button {
+  height: 65px;
+}
+</style>
